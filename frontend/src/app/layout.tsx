a82bf2c83eebf3de2c1a314be00802fc1b62785e
Maria2721 <mashaivvas@gmail.com>
2024-10-08 15:24:40 +0300

    changed global styles, added fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,18 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 
-const inter = Inter({
-	subsets: ["latin", "cyrillic"],
-	variable: "--font-inter",
-	display: "swap",
-});
-const geist = localFont({
-	src: "../../public/fonts/GeistVF.woff",
-	variable: "--font-geist",
-	weight: "100 900",
+const museo = localFont({
+	src: [
+		{ path: "../../public/fonts/MuseoSansCyrl-100.woff", weight: "100" },
+		{ path: "../../public/fonts/MuseoSansCyrl-300.woff", weight: "300" },
+		{ path: "../../public/fonts/MuseoSansCyrl-500.woff", weight: "500" },
+		{ path: "../../public/fonts/MuseoSansCyrl-700.woff", weight: "700" },
+		{ path: "../../public/fonts/MuseoSansCyrl-900.woff", weight: "900" },
+	],
+	variable: "--font-museo",
 	display: "swap",
 });
 
@@ -29,7 +28,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.variable} ${geist.variable} container`}>
+			<body className={`${museo.variable} container`}>
 				<Header />
 				{children}
 				<Footer />
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,14 +6,7 @@ const HomePage = () => {
 	return (
 		<div className={styles.home}>
 			<main className={styles.main}>
-				<Image
-					className={styles.logo}
-					src="https://nextjs.org/icons/next.svg"
-					alt="Next.js logo"
-					width={180}
-					height={38}
-					priority
-				/>
+				HomePage
 				<div className={styles.imgContainer}>
 					<Image
 						src={arrow}
